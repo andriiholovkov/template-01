5b47597d70b4c65791dd65bdfbfca8342ab94e58
Andrii <andriiholovkov@gmail.com>
2022-05-07 16:50:48 +0300

    config(prettier): add prettier package

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  minItems: 3,
+  bracketSameLine: false,
+  bracketSpacing: true,
+  useTabs: false,
+};
